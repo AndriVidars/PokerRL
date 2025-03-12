c2516a86e570e2fa3ed67ea68555df8b75892e5d
AndriVidars <andriv@stanford.edu>
2025-03-11 22:01:05 -0700

    add training runs to .sh

--- train_reinforce.sh
+++ train_reinforce.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
 
-python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.2
-python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.5 --replay_buffer_cap 1000
-python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.25 --replay_buffer_cap 1000
-python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 4000 --lr 5e-5 --max_grad_norm 0.5
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.1
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.25
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.05
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-6 --max_grad_norm 0.1
 
-python -m poker.policy_training.reinforce --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.25
-python -m poker.policy_training.reinforce --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-4 --max_grad_norm 0.5 --replay_buffer_cap 5000
-python -m poker.policy_training.reinforce --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.25 --replay_buffer_cap 1000
-python -m poker.policy_training.reinforce --num_R_players 2 --num_D_players 2 --num_games 4000 --lr 5e-5 --max_grad_norm 0.5
 
-python -m poker.policy_training.reinforce --num_DF_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.2
-python -m poker.policy_training.reinforce --num_DF_players 2 --num_D_players 2 --num_games 4000
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.1
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.25
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.05
 
-python -m poker.policy_training.reinforce --num_H_players 1 --num_DF_players 1 --num_D_players 2 --num_games 4000
-python -m poker.policy_training.reinforce --num_R_players 1 --num_DF_players 1 --num_D_players 2 --num_games 4000
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.1 --replay_buffer_cap 1000
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.25 --replay_buffer_cap 1000
+python -m poker.policy_training.reinforce --num_H_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.05 --replay_buffer_cap 1000
+
+
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.1
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.25
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.05
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 5e-6 --max_grad_norm 0.1
+
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.1
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.25
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 5e-5 --max_grad_norm 0.05
+
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.1 --replay_buffer_cap 1000
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.25 --replay_buffer_cap 1000
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 2 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.05 --replay_buffer_cap 1000
+
+
+python -m poker.policy_training.reinforce --num_DF_players 2 --num_H_players 0 --num_D_players 2 --num_games 2000 --lr 1e-5 --max_grad_norm 0.1
+python -m poker.policy_training.reinforce --num_DF_players 2 --num_H_players 0 --num_D_players 2 --num_games 2000
+
+python -m poker.policy_training.reinforce --num_H_players 1 --num_DF_players 1 --num_D_players 2 --num_games 2000
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 1 --num_DF_players 1 --num_D_players 2 --num_games 2000
+
+python -m poker.policy_training.reinforce --num_H_players 1 --num_DF_players 1 --num_D_players 2 --num_games 2000 --lr 1e-5
+python -m poker.policy_training.reinforce --num_H_players 0 --num_R_players 1 --num_DF_players 1 --num_D_players 2 --num_games 2000 --lr 1e-5
