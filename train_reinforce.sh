4022f6ae9579b19084decab93624e902332249f6
AndriVidars <andriv@stanford.edu>
2025-03-16 19:12:05 -0700

    .

--- train_reinforce.sh
+++ train_reinforce.sh
@@ -3,15 +3,15 @@
 
 # train against itself
 # R2 training
-python -m poker.policy_training.reinforce --validation_players R2.st 3 --num_D_players 1 --num_games 4000
-python -m poker.policy_training.reinforce --validation_players R2.st 2 --num_games 4000
+python -m poker.policy_training.reinforce --validation_players R1.st 3 --num_D_players 1 --num_games 4000
+python -m poker.policy_training.reinforce --validation_players R1.st 2 --num_games 4000
 
 
-python -m poker.policy_training.reinforce --validation_players R2.st 3 --num_D_players 1 --num_games 4000 --lr 5e-6 --max_grad_norm 0.1 
-python -m poker.policy_training.reinforce --validation_players R2.st 2 --num_games 4000  --lr 5e-5 --max_grad_norm 1
+python -m poker.policy_training.reinforce --validation_players R1.st 3 --num_D_players 1 --num_games 4000 --lr 5e-6 --max_grad_norm 0.1 
+python -m poker.policy_training.reinforce --validation_players R1.st 2 --num_games 4000  --lr 5e-5 --max_grad_norm 1
 
 # with heuristic and random
-python -m poker.policy_training.reinforce --validation_players R2.st 1 --num_D_players 1 --num_H_players 2 --num_games 4000
-python -m poker.policy_training.reinforce --validation_players R2.st 1 --num_D_players 1 --num_H_players 1 --num_R_players 1 --num_games 4000
+python -m poker.policy_training.reinforce --validation_players R1.st 1 --num_D_players 1 --num_H_players 2 --num_games 4000
+python -m poker.policy_training.reinforce --validation_players R1.st 1 --num_D_players 1 --num_H_players 1 --num_R_players 1 --num_games 4000
 
 
